4fa36230db150889d36ce846971b8daee9ad281a
em-shea <c.emilyshea@gmail.com>
2020-02-19 22:09:07 -0800

    initial commit

---
+++ GetCats/GetCats.py
@@ -0,0 +1,19 @@
+import io
+import os
+import csv
+import boto3
+from random import randint
+
+cat_list = None
+
+def get_cats():
+
+    s3 = boto3.client('s3')
+
+    csv_file = s3.get_object(Bucket=os.environ['WORDS_BUCKET_NAME'], Key=os.environ['WORDS_BUCKET_KEY'])
+    csv_response = csv_file['Body'].read()
+    stream = io.StringIO(csv_response)
+    reader = csv.DictReader(stream)
+
+    for row in reader:
+        print(reader)
