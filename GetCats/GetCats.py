a986cffba59234b98e18236c8b19138446c22aa8
em-shea <c.emilyshea@gmail.com>
2020-02-19 23:09:39 -0800

    add function return

--- GetCats/GetCats.py
+++ GetCats/GetCats.py
@@ -12,8 +12,18 @@ def lambda_handler(event, context):
 
     csv_file = s3.get_object(Bucket=os.environ['WORDS_BUCKET_NAME'], Key=os.environ['WORDS_BUCKET_KEY'])
     csv_response = csv_file['Body'].read()
-    stream = io.StringIO(csv_response)
+    stream = io.StringIO(csv_response.decode("utf-8"))
     reader = csv.DictReader(stream)
 
     for row in reader:
-        print(reader) 
+        cat_list.append(row)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
+            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
+            'Access-Control-Allow-Origin': '*',
+        },
+        'body': json.dumps(cat_list)
+    }
