417400f91841266c80cddbc68c9aead50739380e
em-shea <c.emilyshea@gmail.com>
2020-03-06 14:27:30 -0800

    simplify cats.py

--- api/cats.py
+++ api/cats.py
@@ -6,36 +6,24 @@ import boto3
 
 def list_cats_handler(event, context):
 
-    status_code, cat_list = read_s3_data(list_cats)
+    status_code, cat_list = read_s3_data()
 
-    return {
-        'statusCode': status_code,
-        'headers': {
-            'Access-Control-Allow-Methods': 'GET,OPTIONS',
-            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-            'Access-Control-Allow-Origin': '*',
-        },
-        'body': json.dumps(cat_list)
-    }
+    response = create_response(status_code, cat_list)
+
+    return response
 
 def get_cat_by_id_handler(event, context):
 
     cat_id = event['pathParameters']['id']
 
-    status_code, response = read_s3_data(get_cat_by_id, cat_id)
+    status_code, cat_found_response = read_s3_data(cat_id)
 
-    return {
-        'statusCode': status_code,
-        'headers': {
-            'Access-Control-Allow-Methods': 'GET,OPTIONS',
-            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-            'Access-Control-Allow-Origin': '*',
-        },
-        'body': json.dumps(response)
-    }
+    response = create_response(status_code, cat_found_response)
+
+    return reponse
 
 #cat_id only passed for get_cat_by_id type requests
-def read_s3_data(request_type, cat_id):
+def read_s3_data(cat_id=None):
 
     s3 = boto3.client('s3')
 
@@ -44,24 +32,30 @@ def read_s3_data(request_type, cat_id):
     stream = io.StringIO(csv_response.decode("utf-8"))
     reader = csv.DictReader(stream)
 
-    if request_type == list_cats:
-        cat_list = []
-        for row in reader:
+    cat_list = []
+    for row in reader:
+        if cat_id is None or row['CatId'] == cat_id:
             # Example image link: https://neko-atsume.s3.amazonaws.com/img/Aluminum+Pins.jpg
             row['CatImage'] = "https://neko-atsume.s3.amazonaws.com/img/" + row['CatImage'].replace(" ", "+")
             row['MementoImage'] = "https://neko-atsume.s3.amazonaws.com/img/" + row['MementoImage'].replace(" ", "+")
             cat_list.append(row)
-        # return status code and API results
+    if cat_id is None:
         return 200, cat_list
-    
-    if request_type == get_cat_by_id:
-        for row in reader:
-            if row['CatId'] == cat_id:
-                # Example image link: https://neko-atsume.s3.amazonaws.com/img/Aluminum+Pins.jpg
-                row['CatImage'] = "https://neko-atsume.s3.amazonaws.com/img/" + row['CatImage'].replace(" ", "+")
-                row['MementoImage'] = "https://neko-atsume.s3.amazonaws.com/img/" + row['MementoImage'].replace(" ", "+")
-                # return status code and API results
-                return 200, row
-        # if no cats found by id, return error code
-        error_message = {"error":"Cat not found. Enter an id between 1-66."}
-        return 404, error_message
+    else:
+        if len(cat_list) = 1:
+            return 200, cat_list[0]
+        else:
+            error_message = {"error":"Cat not found. Enter an id between 1-66."}
+            return 404, error_message 
+
+def create_response(status_code, response_body):
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
+            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
+            'Access-Control-Allow-Origin': '*',
+        },
+        'body': json.dumps(response_body)
+    }
