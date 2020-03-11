01e4c75e127d6eed1c5243b48f4e3dd332dd6bd7
em-shea <c.emilyshea@gmail.com>
2020-03-10 17:33:21 -0700

    add goodies s3 bucket info

--- api/get_items.py
+++ api/get_items.py
@@ -54,23 +54,28 @@ def get_goodies_by_id_handler(event, context):
 
 def read_s3_data(object_type, item_id=None):
 
-    s3 = boto3.client('s3')
-
-    csv_file = s3.get_object(Bucket=os.environ['WORDS_BUCKET_NAME'], Key=os.environ['WORDS_BUCKET_KEY'])
-    csv_response = csv_file['Body'].read()
-    stream = io.StringIO(csv_response.decode("utf-8"))
-    reader = csv.DictReader(stream)
-
     if object_type = "cat":
+        s3_bucket_name = os.environ['CATS_BUCKET_NAME']
+        s3_bucket_key = os.environ['CATS_BUCKET_KEY']
         id_row = row['CatId']
         name_row = row['CatName']
         image_transform_function = cat_image_transform
         error_message = "Cat not found. Request a valid cat name (not case sensitive) or cat id (between 1-66)."
+
     else:
+        s3_bucket_name = os.environ['GOODIES_BUCKET_NAME']
+        s3_bucket_key = os.environ['GOODIES_BUCKET_KEY']
         id_row = row['GoodyId']
         name_row = row['GoodyName']
         image_transform_function = goody_image_transform
         error_message = "Goody not found. Request a valid goody name (not case sensitive) or goody id (between 1-185)."
+
+    s3 = boto3.client('s3')
+
+    csv_file = s3.get_object(Bucket=s3_bucket_name, Key=s3_bucket_key)
+    csv_response = csv_file['Body'].read()
+    stream = io.StringIO(csv_response.decode("utf-8"))
+    reader = csv.DictReader(stream)
 
     item_list = []
     for row in reader:
